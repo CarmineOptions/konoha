f3bf61f82ef6c374fcb0e477cc8824ec8cfd055b
Ondřej Sojka <ondrej.sojka@gmail.com>
2024-08-27 15:08:32 +0200

    Polish token balance code

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,8 +1,9 @@
 export const CONTRACT_ADDR =
 	// mainnet: "0x001405ab78ab6ec90fba09e6116f373cda53b0ba557789a4578d8c1ec374ba0f";
 	"0x02ba6e05d06a9e7398ae71c330b018415f93710c58e99fb04fa761f97712ec76"; // sepolia with treasury
-export const TOKEN_CONTRACT =
-	"0x2b91dd683bc4bcae7a9c5d0cbeba0e8d62fa657742e4640f1e8163dc10e9bd7";
+export const VOTING_TOKEN_CONTRACT =
+	"0x4ff1af47bb9659aa83bbd33e13c25e8fb1b5ecf8359320251f03e1440e8890a";
+export const FLOATING_TOKEN_CONTRACT = "0x31868056874ad7629055ddd00eb0931cb92167851702abf6b441cb8ea02d02b";
 
 export const formatAddress = (addr: string) =>
 	addr.length === 66
