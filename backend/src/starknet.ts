58dff7540288a31be96dd472eef6754071813393
djeck1432 <oleh.kostashchuk@gmail.com>
2024-09-06 22:03:04 +0200

    add new logic for getEvents to return vesting

--- backend/src/starknet.ts
+++ backend/src/starknet.ts
@@ -1,24 +1,86 @@
 import { StarknetIdNavigator } from "starknetid.js";
-import { RpcProvider, constants } from "starknet";
+import {RpcProvider, constants, starknetId} from "starknet";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const NODE_URL: string = process.env.NODE_URL || "";
+const CHAIN_ID: string = process.env.NETWORK === 'mainnet'
+    ? constants.StarknetChainId.SN_MAIN
+    : constants.StarknetChainId.SN_SEPOLIA;
+
+const provider = new RpcProvider({
+  nodeUrl: NODE_URL,
+  chainId: CHAIN_ID,
+});
+
+const starknetIdNavigator = new StarknetIdNavigator(
+    provider,
+    CHAIN_ID,
+);
+
+export const getVestingEvents = async (address: string) => {
+  try {
+    const eventFilter = {
+      from_block: { block_number: 0 },
+      chunk_size: 1000,
+      address: address,
+      key: ['Vesting'],
+    };
+
+    const events = await provider.getEvents(eventFilter);
+
+    // Check if events are empty and return an empty array
+    if (!events.events || events.events.length === 0) {
+      return [];
+    }
+
+    const now = Math.floor(Date.now() / 1000); // Get current timestamp in seconds
+
+    const results = events.events.reduce((acc: any[], event: any) => {
+      try {
+        // VEST: {grantee, timestamp, amount}
+        const grantee = event.data[0];         // Grantee (index 0)
+        const timestamp = parseInt(event.data[1]);  // Timestamp (index 1)
+        const amount = parseInt(event.data[2]);     // Amount (index 2)
+
+        // Processing based on the timestamp
+        if (timestamp < now) {
+          acc.push({
+            grantee: grantee,
+            amount: amount,
+            is_claimable: false
+          });
+        } else if (timestamp > now && amount) {
+          acc.push({
+            grantee: grantee,
+            amount: 0,
+            is_claimable: false
+          });
+        } else {
+          acc.push({
+            grantee: grantee,
+            amount: amount,
+            is_claimable: true
+          });
+        }
+      } catch (error) {
+        // Log the error and continue with the next event
+        console.error('Error processing event, skipping this event:', error);
+      }
+      return acc;
+    }, []);
+
+    return results;
+  } catch (error) {
+    console.error('Error in getVestingEvents:', error);
+    throw new Error(`Error fetching events: ${error}`);
+  }
+};
 
 export const getStarknetId = async (
-  address: string
+    address: string
 ): Promise<string | null> => {
-  const provider = new RpcProvider({
-    nodeUrl: NODE_URL,
-    chainId: constants.StarknetChainId.SN_MAIN
-  });
-
-  const starknetIdNavigator = new StarknetIdNavigator(
-    provider,
-    constants.StarknetChainId.SN_MAIN
-  );
-
   try {
     const domain = await starknetIdNavigator.getStarkName(address);
     const id = await starknetIdNavigator.getStarknetId(domain);
