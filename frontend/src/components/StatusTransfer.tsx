c7407a4ba2ba39622c2f0b09fe1e26c9077cfb8e
Oleh Zorin <real.mad0k@gmail.com>
2024-09-20 15:51:07 +0300

    render live transfers

--- frontend/src/components/StatusTransfer.tsx
+++ frontend/src/components/StatusTransfer.tsx
@@ -1,67 +1,90 @@
 import React, {useEffect} from "react";
 import {
+    useAccount,
     useContractRead
 } from "@starknet-react/core";
-import {TREASURY_ADDRESS} from "../constants/config.json";
 import {TreasuryABI} from "../lib/treasuryABI";
 import CancelTransferBtn from "./CancelTransferBtn";
 import {formatBalance} from "../lib/erc20";
+import {TREASURY_ADDRESS} from "../lib/config";
 
 const StatusTransfer = () => {
-
-    const getLiveTransfer = useContractRead({
+    const { address } = useAccount()
+    const {data, isLoading, refetch} = useContractRead({
         functionName: 'get_live_transfers',
         address: TREASURY_ADDRESS,
         abi: TreasuryABI,
+        watch: false,
+        retry: false
     })
 
+    const renderCancelBtn =  (status, transfer_id) =>
+        getTransferStatus(status) == 'PENDING' && address &&
+      <CancelTransferBtn transferId={transfer_id.toString()} />
+
+    const renderData = () => {
+        if (Array.isArray(data)) {
+            if (data.length > 0) {
+                return <div className='p-2'>There are no transfers</div>;
+            }
+
+            return data.map((transfer_item, index) => {
+
+                return (
+                    <div key={index} className="flex flex-wrap flex-row">
+                        <div className='flex flex-wrap flex-row w-full'>
+                            <div className="flex-1 w-1/2 basis-1/2 p-2 bg-slate-200 text-left text-gray-700 uppercase tracking-wider border-r border-slate-400">token</div>
+                            <div className="flex-1 w-1/2 basis-1/2 p-2 bg-slate-200 text-left text-gray-700 uppercase tracking-wider">reciver</div>
+                            <div className="flex-1 w-1/2 basis-1/2 p-2 overflow-hidden">{transfer_item.token_addr.toString()}</div>
+                            <div className="flex-1 w-1/2 basis-1/2 p-2 overflow-hidden">{transfer_item.receiver.toString()}</div>
+                        </div>
+                        <div className='flex flex-wrap flex-row w-full'>
+                            <div className="flex-1 w-1/4 basis-1/4 p-2 bg-slate-200 text-left text-gray-700 uppercase tracking-wider">amount</div>
+                            <div className="flex-1 w-1/4 basis-1/4 p-2 bg-slate-200 text-left text-gray-700 uppercase tracking-wider">cooldown_end</div>
+                            <div className="flex-1 w-1/4 basis-1/4 p-2 bg-slate-200 text-left text-gray-700 uppercase tracking-wider">status</div>
+                            <div className="flex-1 w-1/4 basis-1/4 p-2 bg-slate-200 text-left text-gray-700 uppercase tracking-wider">actions</div>
+                            <div className="flex-1 w-1/4 basis-1/4 p-2 ">{formatBalance(transfer_item.amount.toString())}</div>
+                            <div className="flex-1 w-1/4 basis-1/4 p-2 ">{getHoursLeft(transfer_item.cooldown_end.toString())}</div>
+                            <div className="flex-1 w-1/4 basis-1/4 p-2 ">{getTransferStatus(transfer_item.status)}</div>
+                            <div className="flex-1 w-1/4 basis-1/4 p-2 ">
+                                { renderCancelBtn(transfer_item?.status, transfer_item?.id) }
+                            </div>
+                        </div>
+
+                    </div>
+                )
+            })
+        } else {
+            return null
+        }
+    }
+
+
     useEffect(() => {
-        getLiveTransfer.refetch()
+        refetch()
     },[])
 
-
-    const data = Transfers_mock
     return (
         <div>
             <div className="flex w-full flex-grow pb-4 text-2xl font-bold">Transfer status</div>
+            {isLoading ? (
+                    <div>Loading...</div>
+                ) :(
+            <div className="w-[50rem] max-w-[50rem] grid  items-center pl-0 rounded-lg bg-slate-200">
+                <div className="min-w-full bg-white border border-gray-200 rounded-lg">
 
-            <div className="w-[50rem] max-w-[50rem] grid  items-center p-2 pl-0 rounded-lg bg-slate-200">
-                <div className="p-2  grid grid-cols-6 font-bold">
-                    <div className="grid gap-20">token</div>
-                    <div className="grid gap-20">receiver</div>
-                    <div className="grid gap-20">amount</div>
-                    <div className="grid gap-20">cooldown</div>
-                    <div className="grid gap-20">status</div>
-                    <div className="grid gap-20"></div>
-                </div>
-                {data && data.map((transfer_item) => (
-                    <div key={transfer_item.id} className="p-2  grid grid-cols-6">
-                        <div className="grid gap-20">{transfer_item.token_addr}</div>
-                        <div className="grid gap-20">{transfer_item.receiver}</div>
-                        <div className="grid gap-20">{formatBalance(BigInt(transfer_item.amount))}</div>
-                        <div className="grid gap-20">{getHoursLeft(Number(transfer_item.cooldown_end))}</div>
-                        <div className="grid gap-20">{transfer_item.status}</div>
-                        <div className="grid gap-20">
-                            <CancelTransferBtn transferId={transfer_item.id} />
-                        </div>
+                    <div>
+                        {renderData()}
                     </div>
-                    ))
-                }
+                </div>
             </div>
+                )}
         </div>
     )
 }
 
-const Transfers_mock = [
-    {id: 1, token_addr: '123123', receiver: '321231', amount: '28629396609133588254', cooldown_end: '1726851313', status:'PENDING'},
-    {id: 2, token_addr: '123123', receiver: '321231', amount: '28629396609133588254', cooldown_end: '1726851313', status:'PENDING'},
-    {id: 3, token_addr: '123123', receiver: '321231', amount: '28629396609133588254', cooldown_end: '1726851313', status:'PENDING'},
-    {id: 4, token_addr: '123123', receiver: '321231', amount: '28629396609133588254', cooldown_end: '1726851313', status:'PENDING'},
-    {id: 5, token_addr: '123123', receiver: '321231', amount: '28629396609133588254', cooldown_end: '1726851313', status:'PENDING'},
-    ]
-
 const getHoursLeft = (target_date: number): string => {
-    const targetTimestamp = target_date * 1000;
+    const targetTimestamp = Number(target_date) * 1000;
     const currentTimestamp = Date.now();
 
     const timeLeft = targetTimestamp - currentTimestamp;
@@ -74,5 +97,18 @@ const getHoursLeft = (target_date: number): string => {
         return 'expired';
     }
 };
+
+const getTransferStatus = status => {
+    if (status?.variant?.PENDING) {
+        return 'PENDING'
+    }
+    if (status?.variant?.CANCELLED) {
+        return 'CANCELLED'
+    }
+    if (status?.variant?.FINISHED) {
+        return 'FINISHED'
+    }
+
+}
 
 export default StatusTransfer
