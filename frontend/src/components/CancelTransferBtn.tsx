d09aa7684eaf94945079f9d00fc15a885394f4c3
Oleh Zorin <real.mad0k@gmail.com>
2024-09-20 15:52:19 +0300

    logical cancel transfer

--- frontend/src/components/CancelTransferBtn.tsx
+++ frontend/src/components/CancelTransferBtn.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {useContractWrite} from "@starknet-react/core";
-import {TREASURY_ADDRESS} from "../constants/config.json";
+import {TREASURY_ADDRESS} from "../lib/config";
+import {Contract} from "starknet";
+import TreasuryABI from "../lib/treasury_abi.json";
 
 interface CancelTransferButtonProps {
     transferId: number;
@@ -8,17 +10,17 @@ interface CancelTransferButtonProps {
 
 const CancelTransferBtn: React.FC<CancelTransferButtonProps> = ({transferId}) => {
 
-    const { write, error } = useContractWrite({
-        calls: [{
-            contractAddress: TREASURY_ADDRESS,
-            entrypoint: 'cancel_transfer',
-            calldata: [transferId],
-        }],
+    const contract = new Contract(TreasuryABI, TREASURY_ADDRESS)
+
+    const { writeAsync: write_yes , error } = useContractWrite({
+        calls: [
+            contract.populateTransaction["cancel_transfer"](transferId)
+        ],
     });
 
     const handleCancelTransfer = async () => {
         try {
-            await write();
+            await write_yes();
             console.log(`Transfer ${transferId} cancelled successfully.`);
         } catch (err) {
             console.error(`Error cancelling transfer ${transferId}:`, err);
@@ -29,7 +31,7 @@ const CancelTransferBtn: React.FC<CancelTransferButtonProps> = ({transferId}) =>
     return (
         <button
             type="button"
-            className='bg-blue-500 rounded-md text-white disabled:opacity-50'
+            className='bg-blue-500 rounded-md text-white disabled:opacity-50 p-1 w-full'
             onClick={handleCancelTransfer}>
             cancel
         </button>
